24cc90a66d9d626e20a7b649d67893309e2b0c5b
simon <sidit77@gmail.com>
2016-01-19 18:56:25 +0100

    Specular lighting

--- src/main/resources/assets/shader/Fragment.glsl
+++ src/main/resources/assets/shader/Fragment.glsl
@@ -8,9 +8,16 @@ in GS_OUT{
     float visibility;
 } fs_in;
 
-uniform bool fog = false;
-
 layout(binding = 0) uniform sampler2D grass;
+
+uniform vec3 pos;
+
+uniform bool fog = false;
+uniform vec3 lightDir = vec3(-0.5,1,-0.5);
+uniform float shineDamper = 3;
+uniform float reflectivity = 0.25;
+
+
 
 void main() {
     vec3 blend_weights = abs(fs_in.normal);
@@ -22,14 +29,17 @@ void main() {
                          texture(grass, fs_in.position.zx) * blend_weights.y +
                          texture(grass, fs_in.position.xy) * blend_weights.z;
 
-    float light = 0.9 * clamp(dot(fs_in.normal,vec3(-0.5,1,-0.5)),0.35,1.0);
-    
+    float diffuselight = 0.9 * clamp(dot(fs_in.normal,lightDir),0.35,1.0);
+
+
+    float specularLight = reflectivity * pow(max(dot(reflect(lightDir, fs_in.normal), normalize(fs_in.position - pos)),0),shineDamper);
+
     //vec3 color = mix(vec3(0.5,0.5,0.5),vec3(0.25,0.6,0.25),clamp(dot(fs_in.normal, vec3(0,1,0))-0.25,0.0,1.0));
     //vec3 color = vec3((sin(fs_in.position.x/5)+1)/2,(cos(fs_in.position.y/5)+1)/2,(sin(fs_in.position.z/5)+1)/2) * 0.7f + vec3(0.3, 0.3, 0.3);
     vec4 color = vec4(0.25,0.6,0.25,1) * blended_color;
     //color = floor(color * 5) * 0.2;
     //light = floor(light * 5) * 0.2;
-    pixel = color * light;//vec4(color * light,1);
+    pixel = color * diffuselight + color * specularLight;//vec4(color * light,1);
     if(fog){
         pixel = mix(vec4(0.7,0.7,0.7,1), pixel, fs_in.visibility);
     }
